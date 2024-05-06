e7c7c8788c37bbfa2d1a8e34a09614ffa965e819
TreskovYuri <169077900+TreskovYuri@users.noreply.github.com>
2024-05-06 20:58:53 +0300

    qsq

--- components/SuperAdmin/Sidebar/Sidebar.js
+++ components/SuperAdmin/Sidebar/Sidebar.js
@@ -51,7 +51,7 @@ const Sidebar = observer(() => {
         <Link href='/admin/consultation' className={mobx.page==='Консультации'?`${css.link} ${css.active}`:`${css.link}`}><Image src={img9} className={css.img}/>Консультации</Link> */}
       </div>
       <div className={css.exitContainer}>
-        <Link href='/admin/settings' className={mobx.page==='settings'?`${css.link} ${css.active}`:`${css.link}`}><Image src={settings} className={css.img} onClick={()=>mobx.setPage('settings')}/>Настройки</Link>
+        <Link href='/' className={mobx.page==='settings'?`${css.link} ${css.active}`:`${css.link}`}><Image src={settings} className={css.img} onClick={()=>mobx.setPage('settings')}/>Настройки</Link>
         <div onClick={exit} className={`${css.link} ${css.exit}`}><Image src={ex} className={css.img}/>Выход</div>
       </div>
 
